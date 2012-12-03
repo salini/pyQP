7957b7a87008972ec985aca1e395644e90458f2d
salini <josephsalini@gmail.com>
2012-12-03 21:36:18 +0100

    fix building problem, should now work on ubuntu and windows

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 
 # System imports
-from distutils.core import *
+from distutils.core import setup, Extension
+from distutils.command.build_py import build_py
 
 # Third-party modules - we depend on numpy for everything
 import numpy
@@ -13,20 +14,23 @@ except AttributeError:
     numpy_include = numpy.get_numpy_include()
 
 # pyQP extension module
-_pyQP = Extension("pyQP/_pyQP",
+_pyQP = Extension("pyQP._pyQP",
                    ["pyQP/QuadProg++.cpp","pyQP/Array.cpp","pyQP/pyQP.i"],
                    include_dirs = ['pyQP', numpy_include],
                    swig_opts = ["-c++"],
                    )
 
 # py setup
-setup(  name        = "pyQP",
+dist = setup(  name        = "pyQP",
         description = "Swig of QuadProg++, quadratic solver using an active-set dual method.",
         author      = "Joseph Salini",
         url         = 'https://github.com/salini/pyQP',
         version     = "0.1",
         ext_modules = [_pyQP],
-        py_modules  = ["pyQP/pyQP", "pyQP/__init__"],
+        packages    = ["pyQP"],
         )
 
-
+# Rerun the build_py to ensure that swig generated py files are also copied
+build_py = build_py(dist)
+build_py.ensure_finalized()
+build_py.run()
